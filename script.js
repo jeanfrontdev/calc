6a32993f8ccb1a622bb92bf87f9ac0687908a706
Jean <102455180+jeanfrontdev@users.noreply.github.com>
2022-04-27 11:30:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let result=document.getElementById("inputtext");
+
+let calculate=(number)=>{
+    result.value+=number;
+}
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("Enter the valid input");
+    }
+}
+
+function clr(){
+    result.value= " ";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1)
+}
